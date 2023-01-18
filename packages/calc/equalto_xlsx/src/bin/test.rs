7ccfb0d53ffefa92bf0f7a09f92ffe5b440266ff
Nicolás Hatcher <nicolas.hatcher@equalto.com>
2023-01-18 15:42:31 +0100

    UPDATE: Updates the unused test binary to save a copy. (#75)
    This is meant only for Manu's testing and should be superseded
    eventually by an official EqualTo CLI.

--- packages/calc/equalto_xlsx/src/bin/test.rs
+++ packages/calc/equalto_xlsx/src/bin/test.rs
@@ -1,14 +1,31 @@
-//! Test a n Excel xlsx file.
+//! Tests an Excel xlsx file.
 //! Returns a list of differences in json format.
+//! Saves an EqualTo version
+//! This is primary for QA internal testing and will be superseded by an official
+//! EqualTo CLI.
 //!
 //! Usage: test file.xlsx
 
-use equalto_xlsx::compare::test_file;
+use std::path;
+
+use equalto_xlsx::{compare::test_file, export::save_to_xlsx, import::load_model_from_xlsx};
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 2 {
         panic!("Usage: {} <file.xlsx>", args[0]);
     }
-    assert!(test_file(&args[1]).is_ok());
+    // first test the file
+    let file_name = &args[1];
+    println!("Testing file: {file_name}");
+    assert!(test_file(file_name).is_ok());
+
+    // save a copy my_xlsx_file.xlsx => my_xlsx_file.output.xlsx
+    let file_path = path::Path::new(file_name);
+    let base_name = file_path.file_stem().unwrap().to_str().unwrap();
+    let output_file_name = &format!("{base_name}.output.xlsx");
+    let mut model = load_model_from_xlsx(file_name, "en", "UTC").unwrap();
+    model.evaluate();
+    println!("Saving result as: {output_file_name}. Please open with Excel and test.");
+    save_to_xlsx(&model, output_file_name).unwrap();
 }
